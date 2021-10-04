8af8e22bbe1d4a2e678b388b3a687b6f9f33e9e0
tomas145724 <tomas145724@gmail.com>
2021-10-04 19:21:13 -0300

    menu

--- js/script.js
+++ js/script.js
@@ -93,4 +93,38 @@ $(document).ready(function(){
 
     
 });
+//Filtro Portfolio
+
+$('.filter-btn').on('click', function(){
+    let type = $(this).attr('id');
+    let boxes = $('.project-box');
+    $('.main-btn').removeClass('active');
+    $(this).addClass('active');
+
+    if(type == 'video-btn') {
+        eachBoxes('video', boxes);
+      } else if(type == 'dev-btn') {
+        eachBoxes('dev', boxes);
+      } else if(type == 'pb-btn') {
+        eachBoxes('pb', boxes);
+      } else {
+        eachBoxes('all', boxes);
+      }
+
+});
+
+function eachBoxes(type, boxes) {
+
+    if(type == 'all') {
+      $(boxes).fadeIn();
+    } else {
+      $(boxes).each(function() {
+        if(!$(this).hasClass(type)) {
+          $(this).fadeOut('slow');
+        } else {
+          $(this).fadeIn();
+        }
+      });
+    }
+  }
 

11884b6ff1adfb992c1d08871f74abfacdd5d2f7
tomas145724 <tomas145724@gmail.com>
2021-10-17 17:48:50 -0300

    ajustes

--- js/script.js
+++ js/script.js
@@ -151,4 +151,45 @@ function eachBoxes(type, boxes) {
   })
 
 
+  debounce = function(func, wait, immediate) {
+    var timeout;
+    return function() {
+      var context = this, args = arguments;
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  };
+  
+  
+  (function(){
+    var $target = $('.anima'),
+        animationClass = 'anime-area',
+        offset = $(window).height() * 3/5;
+  
+    function animeScroll() {
+      var documentTop = $(document).scrollTop();
+  
+      $target.each(function(){
+        var itemTop = $(this).offset().top;
+        if (documentTop > itemTop - offset) {
+          $(this).addClass(animationClass);
+        } else {
+          $(this).removeClass(animationClass);
+        }
+      });
+    }
+  
+    animeScroll();
+  
+    $(document).scroll(debounce(function(){
+      animeScroll();
+    }, 500));
+  })();
+
 
